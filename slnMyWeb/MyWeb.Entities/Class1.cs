e2383246629e45eca97671d0bb1f5625c58741cd
DESKTOP-BOMEI\bomei <bomeilonch0210@gmail.com>
2020-10-16 00:09:19 +0800

    分層設定

---
+++ slnMyWeb/MyWeb.Entities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWeb.Entities
+{
+    public class Class1
+    {
+    }
+}
