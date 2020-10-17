a60da1f266b87b1d576813838df7bb7013c1de51
DESKTOP-BOMEI\bomei <bomeilonch0210@gmail.com>
2020-10-17 16:08:00 +0800

    刪除功能

--- slnMyWeb/MyWeb.Service/ProductsService.cs
+++ slnMyWeb/MyWeb.Service/ProductsService.cs
@@ -30,13 +30,19 @@ namespace MyWeb.Service
 
         public bool CreateProduct(vw_Products model)
         {
+            int count = 0;
             var config = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<vw_Products, Products>();
             });
             IMapper mapper = config.CreateMapper();
             Products saveModel = mapper.Map<vw_Products, Products>(model);
-            int count = Create(saveModel);
+            try
+            {
+                count = Create(saveModel);
+            }
+            catch (Exception)
+            { }
             if (count == 1)
                 return true;
             else
@@ -45,6 +51,7 @@ namespace MyWeb.Service
 
         public bool UpdateProduct(int id, Products model)
         {
+            int count = 0;
             NorthwindEntities entity = (NorthwindEntities)GetCurrentContext();
             ProductDao dao = new ProductDao();
 
@@ -55,7 +62,26 @@ namespace MyWeb.Service
             });
             IMapper mapper = config.CreateMapper();
             mapper.Map(model, oriProduct);
-            int count= dao.UpdateData(oriProduct, entity);
+            try
+            {
+                count = dao.UpdateData(oriProduct, entity);
+            }
+            catch (Exception)
+            { }
+            if (count == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteProduct(int id)
+        {
+            Products oriProduct = FindOne(m => m.ProductID == id);
+            int count = Delete(oriProduct);
             if (count == 1)
             {
                 return true;
--- slnMyWeb/MyWeb/Areas/Service/Controllers/ProductsController.cs
+++ slnMyWeb/MyWeb/Areas/Service/Controllers/ProductsController.cs
@@ -29,23 +29,69 @@ namespace MyWeb.Areas.Service.Controllers
             vw_Products obj = service.GetProductsList(id).FirstOrDefault();
             return Ok(obj);
         }
+        //public HttpResponseMessage Post(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        // Do something with the product (not shown).
+
+        //        return new HttpResponseMessage(HttpStatusCode.OK);
+        //    }
+        //    else
+        //    {
+        //        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+        //    }
+        //}
         [ValidateAntiForgeryToken]
         public IHttpActionResult Post(vw_Products model)
         {
-            ExecuteResult executeResult = new ExecuteResult();
-            executeResult.Success = service.CreateProduct(model);
-            return Ok(executeResult);
+            if (ModelState.IsValid)
+            {
+                ExecuteResult executeResult = new ExecuteResult();
+                executeResult.Success = service.CreateProduct(model);
+                if (executeResult.Success)
+                {
+                    return Ok(executeResult);
+                }
+                else
+                {
+                    return BadRequest("新增失敗");
+                }
+            }
+            else
+            {
+                //return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                return BadRequest("新增失敗，欄位未填寫完整");
+            }
         }
-
+        [ValidateAntiForgeryToken]
         public IHttpActionResult Put(int id, Products model)
         {
-            ExecuteResult executeResult = new ExecuteResult();
-            executeResult.Success = service.UpdateProduct(id, model);
-            return Ok(executeResult);
+            if (ModelState.IsValid)
+            {
+                ExecuteResult executeResult = new ExecuteResult();
+                executeResult.Success = service.UpdateProduct(id, model);
+                if (executeResult.Success)
+                {
+                    return Ok(executeResult);
+                }
+                else
+                {
+                    return BadRequest("編輯失敗");
+                }
+            }
+            else
+            {
+                //return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                return BadRequest("編輯失敗，欄位未填寫完整");
+            }
         }
 
-        public void Delete(int id)
+        public IHttpActionResult Delete(int id)
         {
+            ExecuteResult executeResult = new ExecuteResult();
+            executeResult.Success = service.DeleteProduct(id);
+            return Ok(executeResult);
         }
     }
 }
